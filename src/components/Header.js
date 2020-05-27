e08a3f314a7d968b9609d1a05a47c05fb058b699
João Vitor Veronese Vieira <joao.vieira@compasso.com.br>
2020-05-26 22:02:43 -0300

    Add profile image on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GetAppIcon from '@material-ui/icons/GetApp';
+import { Avatar } from '@material-ui/core';
 import { myGithub, myLinkedin, appDownload } from '../utils/constants';
+import imageProfile from '../assets/imgs/profile.jpeg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,6 +20,10 @@ const useStyles = makeStyles((theme) => ({
   },
   icon: {
     color: theme.palette.common.white,
+  },
+  medium: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
   },
 }));
 
@@ -39,6 +45,9 @@ const Header = () => {
           </IconButton>
           <IconButton aria-label="github-author" href={myGithub} target="_blank">
             <GitHubIcon className={classes.icon} />
+          </IconButton>
+          <IconButton aria-label="github-author" href={myGithub} target="_blank">
+            <Avatar alt="Remy Sharp" src={imageProfile} className={classes.medium} />
           </IconButton>
         </Toolbar>
       </AppBar>
